641536ce6b2cf4a2b20fd223b43c899594b0da29
HUNGCHEUNGTO <tommyhung56@gmail.com>
2025-07-03 13:44:29 +0800

    Finish lab 1

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,25 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    struct node* hare = head, *tortoise = head;
+
+    if (!head)
+        return 0;
+
+    while (1)
+    {
+        if (hare->next && hare->next->next)
+            hare = hare->next->next;
+        else
+            return 0;
+        
+        if (tortoise->next)
+            tortoise = tortoise->next;
+        
+        if (tortoise == hare)
+            return 1;
+    }
+    
     return 0;
 }
+
